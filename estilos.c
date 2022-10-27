f9f52abf418f2d4f4b7433a19fd7a0310092de44
estudiambre434 <116799258+estudiambre434@users.noreply.github.com>
2022-10-27 04:46:32 -0500

    Add files via upload

---
+++ estilos.c
@@ -0,0 +1,6 @@
+body {
+  heigh:665px;
+  background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nike.com%2Fmx%2F&psig=AOvVaw1RvrtJOOlWBpYgcODyBrx6&ust=1666945642614000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCPDU__Hj__oCFQAAAAAdAAAAABAF");
+  background-size: 10rem;
+  
+}
